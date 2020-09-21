e0732d5103aff4c09d906042482f7a290ee722bb
Mattias Törnqvist <mattias.trnqvist@gmail.com>
2020-09-21 21:43:17 +0200

    Initial Commit

---
+++ Excuse Myself/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Excuse Myself
+//
+//  Created by Mattias Törnqvist on 2020-09-21.
+//  Copyright © 2020 Mattias Törnqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
